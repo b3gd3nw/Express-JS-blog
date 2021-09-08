9fe4bd3d9b3f0d52416ed931c6a69741f8bc9d97
Bogdan Kubitskiy <81744589+k0tk4@users.noreply.github.com>
2021-09-08 12:12:25 +0300

    Delete server directory

--- server/src/server/handlerError/handler.js
+++
@@ -1,7 +0,0 @@
-module.exports.basicErrorHandler = (err, req, res, next) => {
-    if (!err.message || !err.status) {
-      return res.status(500).send('Server Error');
-    }
-    console.log(err);
-    res.status(err.status).send(err.message);
-  };
